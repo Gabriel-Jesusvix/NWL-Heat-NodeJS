7d260795eaea159e642fc6299fff980c6dbbdaab
Gabriel Jesus <62946928+Gabriel-Jesusvix@users.noreply.github.com>
2021-10-20 12:23:25 -0300

    finishing service as backend

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import prismaClient from "../prisma";
 import { sign } from "jsonwebtoken";
 
-interface IAcessTokenResponse {
+interface IAccessTokenResponse {
   access_token: string;
 }
 
@@ -16,20 +16,19 @@ interface IUserResponse {
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
-    const { data: accessTokenResponse } = await axios.post<IAcessTokenResponse>(
-      url,
-      null,
-      {
+
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
         params: {
           client_id: process.env.GITHUB_CLIENT_ID,
-          client_secret: process.env.GITHUB_CLIENTE_SECRET,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
           code,
         },
         headers: {
           Accept: "application/json",
         },
-      }
-    );
+      });
+
     const response = await axios.get<IUserResponse>(
       "https://api.github.com/user",
       {
@@ -38,13 +37,15 @@ class AuthenticateUserService {
         },
       }
     );
-    const { login, id, name, avatar_url } = response.data;
+
+    const { login, id, avatar_url, name } = response.data;
 
     let user = await prismaClient.user.findFirst({
       where: {
         github_id: id,
       },
     });
+
     if (!user) {
       user = await prismaClient.user.create({
         data: {
@@ -70,6 +71,7 @@ class AuthenticateUserService {
         expiresIn: "1d",
       }
     );
+
     return { token, user };
   }
 }
--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../prisma";
 import { io } from "../app";
+
 class CreateMessageService {
   async execute(text: string, user_id: string) {
     const message = await prismaClient.message.create({
